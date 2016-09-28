5e901c0c443015868c8b00bd484eaac75a4f7f98
yych <yych@umich.edu>
2016-09-28 19:08:40 -0400

    nothing

--- fun1.m
+++ fun1.m
@@ -1,6 +1,6 @@
 function [ p ] = fun1( x_j, tau )
 %UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%   Detailed explanation goes here test
 p = exp(-(x_j)^2 / (4 * tau));
 
 end
